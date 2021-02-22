693ae51176b9c0c89be10cddf8561240dedece90
Benjamin Sheth <bsheth@wisc.edu>
2021-02-21 21:08:51 -0600

    Changed check.js to background.js

--- Ben/View Active Tab/background.js
+++ Ben/View Active Tab/background.js
@@ -4,6 +4,7 @@ chrome.tabs.onActivated.addListener(function() {
     },
     function(tabs) {
         var url = tabs[0].url;
+        // TODO add the url to local storage so that viewer.html can access it
         var textNode = document.createTextNode(url);
         var node = document.createElement("LI");
         node.appendChild(textNode);
