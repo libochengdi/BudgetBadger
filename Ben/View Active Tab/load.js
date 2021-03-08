012356709b3b2e7818d735b2c0732d9374d6ff05
Benjamin Sheth <bsheth@wisc.edu>
2021-03-08 03:19:35 -0600

    Fixed unititialized history (again) and commented

--- Ben/View Active Tab/load.js
+++ Ben/View Active Tab/load.js
@@ -1,11 +1,19 @@
+/**
+ * load.js populates popup.html by getting the stored data from local storage
+ * @author Benjamin Sheth
+ */
 chrome.storage.local.get(["items"], function(object) {
-    var history = object.items;
-    for (i = 0; i < history.length; ++i) {
-        var url = "URL: " + history[i].url + " Time: " + history[i].time;
-        var textNode = document.createTextNode(url);
-        var node = document.createElement("LI");
-        node.appendChild(textNode);
-        document.getElementById("myList").appendChild(node);
+    var history = object.items; // history is an array of objects which contain the urls of sites
+                                // you have visited and the time in milliseconds since Jan 1, 
+                                // 1970 00:00:00.000 GMT when it was visited
+    if (typeof(history) !== "undefined") {
+        for (i = 0; i < history.length; ++i) {
+            var url = "URL: " + history[i].url + " Time: " + history[i].time;
+            var textNode = document.createTextNode(url);
+            var node = document.createElement("LI");
+            node.appendChild(textNode);
+            document.getElementById("myList").appendChild(node);
+        }
     }
 });
 
